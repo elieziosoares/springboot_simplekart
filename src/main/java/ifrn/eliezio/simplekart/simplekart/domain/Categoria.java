6b9126ccf2aa1e53f770421658ccf30b44aa7b05
Eliezio Soares <elieziosoares@gmail.com>
2020-09-03 00:42:16 -0300

    Banco H2 JPA e teste em categoria

--- src/main/java/ifrn/eliezio/simplekart/simplekart/domain/Categoria.java
+++ src/main/java/ifrn/eliezio/simplekart/simplekart/domain/Categoria.java
@@ -2,14 +2,23 @@ package ifrn.eliezio.simplekart.simplekart.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GeneratorType;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
+@Entity
 public @Data class Categoria implements Serializable{
     private static final long serialVersionUID = 1L;
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
 }
